9855bcb06444e9ac184fcbcb8f50bc975b492c26
Kevin Horecka <kevin.horecka@gmail.com>
2017-12-22 15:08:43 -0600

    v0.0.1 - Initial Commit
    Basic drag/touch features, and a simple state machine are present. Planned features include:
    -Configuration file support
    -Category stimuli
    -Data logging
    -Scaling/area bounding

---
+++ MSL-iPosition-Task/Assets/Squiggles.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Squiggles : MonoBehaviour
+{
+    public Transform prefab;
+    public Material stimuliMaterial;
+    public Vector2 xRange;
+    public Vector2 yRange;
+    public int randomSeed = 42;
+    public int numItems;
+    private int numberOfStimuli = 150;
+    public float topTestLocationProportion = 0.1f;
+
+    void Start()
+    {
+        UnityEngine.Random.seed = randomSeed;
+
+        RandomizeLocationsAndStimuli();
+    }
+
+    private GameObject[] getParentChildren()
+    {
+        GameObject[] children = new GameObject[transform.childCount];
+        for (int i = 0; i < children.Length; i++)
+            children[i] = transform.GetChild(i).gameObject;
+        return children;
+    }
+
+    public void RandomizeLocationsAndStimuli()
+    {
+        // Destroy old objects (if they exist)
+        GameObject[] children = getParentChildren();
+        for (int i = 0; i < children.Length; i++)
+            DestroyImmediate(children[i]);
+
+        // Make new objects
+        for(var i = 0; i < numItems; i++)
+        {
+            var tex = Resources.Load<Texture2D>("white/" + UnityEngine.Random.Range(1, numberOfStimuli));
+            var obj = Instantiate(prefab) as Transform;
+            obj.SetParent(transform);
+            obj.GetComponent<MeshRenderer>().material = stimuliMaterial;
+            obj.GetComponent<MeshRenderer>().material.mainTexture = tex;
+            obj.localPosition = new Vector3(Random.Range(xRange[0], xRange[1]), Random.Range(yRange[0], yRange[1]), 0);
+        }
+    }
+
+    public void StimuliToTop()
+    {
+        GameObject[] children = getParentChildren();
+        for (int i = 0; i < children.Length; i++)
+            children[i].transform.localPosition = new Vector3(Mathf.Lerp(xRange[0], xRange[1], (float)i / (float)children.Length), yRange[1] - (yRange[1] - yRange[0]) * topTestLocationProportion, 0);
+    }
+}
---
+++ MSL-iPosition-Task/Assets/StateManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TouchScript.Layers;
+using UnityEngine;
+
+public class StateManager : MonoBehaviour {
+    public Squiggles stimuliManager;
+    public StandardLayer touchLayer;
+
+    public enum State { Study, Delay, Test, Finished };
+
+    public State currentState;
+
+    private float startTime;
+    public float studyTime;
+    public float delayTime;
+
+    public KeyCode advanceKey;
+    public int numberOfTrials;
+
+	// Use this for initialization
+	void Start () {
+        currentState = State.Study;
+        startTime = Time.time;
+        touchLayer.enabled = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (currentState == State.Study)
+        {
+            if (Time.time - startTime >= studyTime)
+            {
+                currentState = State.Delay;
+                stimuliManager.gameObject.SetActive(false);
+                startTime = Time.time;
+            }
+        }
+        else if (currentState == State.Delay)
+        {
+            if (Time.time - startTime >= delayTime)
+            {
+                stimuliManager.gameObject.SetActive(true);
+                touchLayer.enabled = true;
+                stimuliManager.StimuliToTop();
+                currentState = State.Test;
+                startTime = Time.time;
+            }
+        }
+        else if (currentState == State.Test)
+        {
+            if (Input.GetKeyUp(advanceKey))
+            {
+                numberOfTrials -= 1;
+                startTime = Time.time;
+                touchLayer.enabled = false;
+                if (numberOfTrials <= 0)
+                {
+                    currentState = State.Finished;
+                }
+                else
+                {
+                    currentState = State.Study;
+                    stimuliManager.RandomizeLocationsAndStimuli();
+                }
+            }
+        }
+	}
+}
