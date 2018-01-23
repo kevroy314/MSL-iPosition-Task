a2f0a4b5cb42062d3d0f25a2cc6a1b26f7498747
kevroy314 <kevin.horecka@gmail.com>
2018-01-22 18:41:23 -0600

    v0.0.6 - Bug Fixes
    -Fixed escape issue
    -Fixed scaling/rotation issue
    -Tried fix for screen border/scaling

---
+++ MSL-iPosition-Task/Assets/Scripts/QuitOnButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitOnButton : MonoBehaviour {
+
+    public KeyCode quitKey = KeyCode.Escape;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(quitKey))
+        {
+            Application.Quit();
+        }
+    }
+}
